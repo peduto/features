eb5af2cb7807148ec15563ba1640f87cfd89329f
Pascoal Peduto <peduto@usp.br>
2018-08-20 16:55:19 -0300

    Incluido a subtração em feature

--- Calculadora.py
+++ Calculadora.py
@@ -5,3 +5,13 @@ num2 = 20.45
 print("Soma desses dois numeros reais é:")
 print(num1+num2)
 
+#Subtração
+
+num1a = 24.45
+num2a = 14.23
+
+print("Subtração de dois numeros reais é:")
+print(num1a-num2a)
+
+
+
